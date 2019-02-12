57b4bcaaed23744444d0c807513ed5d5427d1583
itohin <itoxa@MacBook-Pro.local>
2019-02-12 08:15:46 +0300

    Abstract render class

---
+++ src/Pagination/Renderers/RendererAbstract.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Pagination\Renderers;
+
+use App\Pagination\Meta;
+
+abstract class RendererAbstract
+{
+    /**
+     * @var Meta
+     */
+    protected $meta;
+
+    /**
+     * PlainRenderer constructor.
+     * @param $meta
+     */
+    public function __construct(Meta $meta)
+    {
+        $this->meta = $meta;
+    }
+
+    /**
+     * @return array
+     */
+    public function pages()
+    {
+        $lrCount = 2;
+
+        $range = range(1, $this->meta->lastPage);
+
+        $endDiff = max(0, $this->meta->page - ($this->meta->lastPage - $lrCount) + 1);
+
+        $range = array_slice($range, max(1, ($this->meta->page - $lrCount) - $endDiff));
+        $range = array_slice($range, 0, ($lrCount * 2));
+
+        array_unshift($range, 1);
+        $range[] = $this->meta->lastPage;
+
+        return array_unique($range);
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract public function render();
+}
